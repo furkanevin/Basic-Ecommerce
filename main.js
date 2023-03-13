194475def6843db450480ed3e95b96c688d0cbd2
furkanevin <furkanevin00@gmail.com>
2023-03-13 16:45:55 +0300

    update

--- main.js
+++ main.js
@@ -1,87 +1,139 @@
-const categoriesBox = document.getElementById('categories');
-const productsBox = document.getElementById('products');
-const sepetBtn = document.getElementById('sepetBtn');
-const modal = document.getElementById('modal');
-const closeBtn = document.getElementById('closeModal');
-const toplamBilgi = document.getElementById('toplamBilgi');
-const modalWrapper = document.getElementById('modal-wrapper');
+const categoryList = document.querySelector('.categories');
+const productList = document.querySelector('.products');
+const openBtn = document.getElementById('open-btn');
+const closeBtn = document.getElementById('close-btn');
+const modal = document.getElementById('modal-wrapper');
+const modalInfo = document.getElementById('modal-info');
+const modalList = document.querySelector('.modal-list');
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('https://api.escuelajs.co/api/v1/categories')
-    .then((res) => res.json())
-    .then((data) => {
-      data.slice(0, 4).forEach((category) => {
-        const categoryItem = document.createElement('div');
-
-        categoryItem.classList.add('category');
-
-        categoryItem.innerHTML = `
-        <img src=${category.image}/>
-        <p>${category.name}</p>`;
-
-        categoriesBox.appendChild(categoryItem);
-      });
-    });
+  //CALLBACK > İçerisinde başka fonksiyon çalıştıran fonksiyon
+  fetchCategories();
+  fetchProducts();
 });
 
-//! SEPET
-let sepet = [];
-let fiyatlar = [];
+function fetchCategories() {
+  fetch('https://api.escuelajs.co/api/v1/categories')
+    // GELEN VERİYİ İŞLEME
+    .then((response) => response.json())
+    // OLUŞAN DATAYI FOREACH İLE HERR BİR OBJE İÇİN FONKSİYON ÇALIŞTIRMA
+    .then((data) =>
+      data.slice(0, 4).forEach((category) => {
+        // GELEN HERBİR OBJE İÇİN dıv oluşturma
+        const categoryDıv = document.createElement('div');
+        // Dive class ekleme
+        categoryDıv.classList.add('category');
+        // Divin içeriğini değiştirme
+        categoryDıv.innerHTML = `
+           <img src="${category.image}"/>
+           <span>${category.name}</span>
+        `;
+        // Oluşan categoryi htmldeki listeye atma
+        categoryList.appendChild(categoryDıv);
+      })
+    )
+    .catch((err) => console.log(err));
+}
 
-sepetBtn.addEventListener('click', () => {
-  modalWrapper.classList.add('active');
+// ÜRÜNLERİ ÇEKME
+function fetchProducts() {
+  // apı YE İSTEK ATMA
+  fetch('https://api.escuelajs.co/api/v1/products/') //endpoint
+    // İstek başarılı olursa veriyi işle
+    .then((res) => res.json())
+    // işlenen veriyi al ve ekrana bas
+    .then((data) =>
+      data.slice(0, 25).forEach((product) => {
+        // DİV oluştur
+        const productDıv = document.createElement('div');
+        productDıv.classList.add('product');
+        // içeiriği değiştir
+        productDıv.innerHTML = `
+          <img src="${product.images[0]}" />
+            <p class="product-title">${product.title}</p>
+            <p class="product-category">${product.category.name}</p>
+            <div class="product-action">
+              <p>${product.price} $</p>
+              <button onclick="sepeteEkle({id:'${product.id}',name:'${product.title}',price:'${product.price}',image:'${product.images[0]}',amount:1})">Sepete Ekle</button>
+            </div>
+          </div>
+        `;
+        // htmle göndericez
+        productList.appendChild(productDıv);
+      })
+    )
+    // hata olursa devreye gir
+    .catch();
+}
+// Butonlara tıklanma olayını izliyoruz
+
+let basket = [];
+var toplam = 0;
+
+const addList = () => {
+  basket.forEach((product) => {
+    const listItem = document.createElement('div');
+    listItem.classList.add('list-item');
+    listItem.innerHTML = `
+                  <div>
+                      <img src="${product.image}" alt="">
+                  </div>              
+                  <h2>${product.name}</h2>
+                  <h2>${product.price}</h2>
+                  <p>Miktar: ${product.amount}</p>
+                  <button id="del" onclick="deleteFrom({id:'${product.id}',price:'${product.price}',amount:'${product.amount}'})">Del</button>
+  `;
+    modalList.appendChild(listItem);
+
+    toplam += Number(product.price) * Number(product.amount);
+  });
+};
+
+// SEPETTEN SİL
+const deleteFrom = (param) => {
+  basket = basket.filter((i) => i.id != param.id);
+  toplam -= Number(param.price) * param.amount;
+  modalInfo.innerText = toplam;
+};
+modalList.addEventListener('click', (e) => {
+  if (e.target.id === 'del') {
+    e.target.parentElement.remove();
+  }
+});
+
+openBtn.addEventListener('click', () => {
+  toggleModal();
+  addList();
+  modalInfo.innerText = toplam;
 });
 
 closeBtn.addEventListener('click', () => {
-  modalWrapper.classList.remove('active');
-  sepet = [];
+  toggleModal();
+  modalList.innerHTML = ' ';
+  toplam = 0;
 });
 
-function sepeteEkle(param) {
-  const urunDiv = document.createElement('div');
-  urunDiv.classList.add('modalItem');
-  urunDiv.innerHTML = `
-  <h2>${param.name}</h2>
-  <p>${param.price} tl</p>
-  `;
-  modal.appendChild(urunDiv);
-  fiyatlar.push(+param.price);
-  const toplam = fiyatlar.reduce((a, b) => a + b, 0);
-  toplamBilgi.innerText = toplam;
+// Eğer dışarıya tıklanırsa da kapat
+// modal.addEventListener('click', (e) => {
+//   if (e.target.id !== 'modal') {
+//     modal.classList.remove('active');
+//     modalList.innerHTML = ' ';
+//   }
+// });
+
+// Beğer tıklannırsa class eklenip çıkarılıyor
+function toggleModal() {
+  modal.classList.toggle('active');
 }
 
-function setModal(param) {
-  console.log(param);
+//! SEPETE EKLEME İŞLEMİ
+
+function sepeteEkle(product) {
+  const findItem = basket.find((eleman) => eleman.id === product.id);
+
+  if (findItem) {
+    findItem.amount += 1;
+  } else {
+    basket.push(product);
+  }
 }
-
-//? ÜRÜNLERİ ÇEKME VE YAZMA
-document.addEventListener('DOMContentLoaded', () => {
-  // Event Başlangıç
-  fetch('https://api.escuelajs.co/api/v1/products')
-    .then((res) => res.json())
-    .then((data) => {
-      //her bir veri için döngü
-      data.slice(1, 20).forEach((product) => {
-        //Ürün Oluşturma
-        const productItem = document.createElement('div');
-
-        productItem.classList.add('product');
-
-        productItem.innerHTML = `
-            <img
-              src=${product.images[0]}
-            />
-            <p onclick="setModal(${product.id})" >${product.title}</p>
-
-            <label>${product.category.name}</label>
-
-            <div class="card-buy">
-              <label>${product.price}$</label> 
-              <button onclick="sepeteEkle({name:'${product.title}',price:'${product.price}'})">Sepete Ekle</button>
-            </div>`;
-
-        productsBox.appendChild(productItem);
-      });
-    });
-  // Event Son
-});
